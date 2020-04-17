c501076adf4de8d041d6fad95749d1138d2e5ce8
DeyouKong <1026328997@qq.com>
2020-04-17 19:59:37 +0800

    add something

--- testcase/common.py
+++ testcase/common.py
@@ -84,17 +84,28 @@ def delRedis(r, channel, open_id, union_id, user_id):
         print("出错啦：%s" % msg)
 
 if __name__ == '__main__':
-    env = "staging2"
-    channel = "Z"
-    db = 0
-    r = connectRedis(env, db=db)
 
     """
     支付宝的 open_id 和 union_id 均为 user_oauth 表的 identifier 字段数据
     """
+    run = "Z"
 
-    open_id = "2088502813153020"
-    union_id = "2088502813153020"
-    user_id = "1000000052"
+    if run == "W":
+        env = "staging2"
+        channel = "W"
+        db = 0
+        open_id = "onsAE0S4WSrwHYwBUD-W1ncD1sJg"
+        union_id = "oZ3J71dlZ7rLj2cJf94z_Q2EdJwc"
+        user_id = "22957588"
+    else:
+        env = "staging2"
+        channel = "Z"
+        db = 0
+        open_id = "2088502813153020"
+        union_id = "2088502813153020"
+        user_id = "1000000053"
 
+
+
+    r = connectRedis(env, db=db)
     delRet = delRedis(r, channel, open_id, union_id, user_id)
